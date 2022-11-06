57318dd55fd667718fa1279dcb5605af00bde4a5
chris abdo <chris.j.abdo@gmail.com>
2022-11-05 21:16:41 -0700

    ADDED: Fixes to AppBody.tsx

--- components/AppBody.tsx
+++ components/AppBody.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Head from "next/head";
 
+import toast from "react-hot-toast";
+
 import APIKeyForm from "./APIKeyForm";
 
 import { APP_STATES } from "../utils/types";
@@ -34,11 +36,22 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
     case APP_STATES.CREATE_BUTTON:
       return (
         <div className="w-full h-3/5 flex items-center justify-center">
-          <button
+          {/* <button
             className="text-2xl border border-black rounded p-2"
             onClick={createStream}
           >
             Create Stream
+          </button> */}
+          <button
+            onClick={createStream}
+            className="relative inline-block px-4 py-2  group"
+          >
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#6AB313] group-hover:-translate-x-0 group-hover:-translate-y-0 border-black border-[2px]"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-[#6AB313]"></span>
+
+            <span className="relative text-black group-hover:text-black ">
+              Go Live!
+            </span>
           </button>
         </div>
       );
@@ -99,7 +112,7 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
         2
       );
       return (
-        <div className="container w-full flex flex-col items-center overflow-auto pb-14">
+        <div className="container w-full flex flex-col items-center overflow-hidden pb-14">
           <div
             className="relative bg-black w-full xl:w-3/5 overflow-hidden"
             style={{ minHeight: "400px" }}
@@ -120,43 +133,66 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
             </div>
           </div>
 
-          <div className="w-11/12 lg:w-full xl:w-3/5 lg:p-0 mt-2 text-red-500 text-left text-sm">
-            <span className="font-bold">Note:&nbsp;</span> To start a video
-            stream, please use a broadcaster software like OBS/Streamyard on
-            desktop, or Larix on mobile
+          <div className="w-11/12 lg:w-full xl:w-3/5 lg:p-0 mt-2 text-red-500 text-center text-sm font-bold">
+            <span className="font-bold">Note:&nbsp;</span> OBS Studio is highly
+            recommended for streaming to DeStemr.{" "}
           </div>
-          <div className="w-11/12 lg:w-full xl:w-3/5 border border-dashed p-2 m-4 flex flex-col text-sm">
+          <div className="w-11/12 lg:w-full xl:w-3/5 p-2 m-4 flex flex-col text-sm border-[2px] border-black">
             <div className="flex items-center justify-between mt-2 break-all">
               <span>
-                Ingest URL:
+                <span className="font-bold">Ingest URL:</span>
                 <br />
                 rtmp://rtmp.livepeer.com/live/
               </span>
               <button
                 onClick={() =>
-                  copyTextToClipboard(`rtmp://rtmp.livepeer.com/live/`)
+                  // copyTextToClipboard(`rtmp://rtmp.livepeer.com/live/`)
+
+                  // code as above as also toast success message
+                  toast.success("Ingest URL copied to clipboard!", {
+                    style: {
+                      border: "2px solid #000",
+                      // make bold
+                      fontWeight: "bold",
+                    },
+                  }) && copyTextToClipboard(`rtmp://rtmp.livepeer.com/live/`)
                 }
-                className="border ml-1 p-1 rounded text-sm break-normal"
+                className="border ml-1 p-1 rounded text-sm break-normal hover:bg-gray-400"
               >
                 Copy
               </button>
             </div>
             <div className="flex items-center justify-between mt-2 break-all mb-6">
               <span>
-                Stream Key:
+                <span className="font-bold">Stream Key:</span>
                 <br />
                 {streamKey}
               </span>
               <button
-                onClick={() => copyTextToClipboard(streamKey)}
-                className="border ml-1 p-1 rounded text-sm break-normal"
+                // onClick={() => copyTextToClipboard(streamKey)}
+                // on click copy streamkey to clipboard and also show a toast notification
+                onClick={() => {
+                  copyTextToClipboard(streamKey);
+                  const notification = toast.success(
+                    "Stream Key copied to clipboard!",
+                    {
+                      style: {
+                        border: "2px solid #000",
+                        // make bold
+                        fontWeight: "bold",
+                      },
+                    }
+                  );
+                  notification;
+                }}
+                className="border ml-1 p-1 rounded text-sm break-normal hover:bg-gray-400"
               >
                 Copy
               </button>
             </div>
-            <div className="flex items-center justify-between mt-2 break-all">
+            {/* <div className="flex items-center justify-between mt-2 break-all">
               <span>
-                Playback URL:
+                <span className="font-bold">Playback URL:</span>
                 <br />
                 https://cdn.livepeer.com/hls/{playbackId}/index.m3u8
               </span>
@@ -166,35 +202,48 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
                     `https://cdn.livepeer.com/hls/${playbackId}/index.m3u8`
                   )
                 }
-                className="border ml-1 p-1 rounded text-sm break-normal"
+                className="border ml-1 p-1 rounded text-sm break-normal hover:bg-gray-400"
               >
                 Copy
               </button>
-            </div>
+            </div> */}
             <div className="flex items-center justify-between mt-4 break-all">
               <span>
-                Share video: <br />
-                {`${window.location.href}video/${state.divKey.replace(
+                <span className="font-bold">
+                  Share video: <br />
+                </span>
+                {/* {`${"https://destemrlivepeer.vercel.app/"}video/${state.divKey.replace(
+                  "_",
+                  "-"
+                )}`} */}
+                {`https://destemrlivepeer.vercel.app/video/${state.divKey.replace(
                   "_",
                   "-"
                 )}`}
               </span>
               <button
                 onClick={() =>
+                  toast.success("Stream URL copied to clipboard!", {
+                    style: {
+                      border: "2px solid #000",
+                      // make bold
+                      fontWeight: "bold",
+                    },
+                  }) &&
                   copyTextToClipboard(
-                    `${window.location.href}video/${state.divKey.replace(
+                    `https://destemrlivepeer.vercel.app/video/${state.divKey.replace(
                       "_",
                       "-"
                     )}`
                   )
                 }
-                className="border ml-1 p-1 rounded text-sm break-normal"
+                className="border ml-1 p-1 rounded text-sm break-normal hover:bg-gray-400"
               >
                 Copy
               </button>
             </div>
           </div>
-          <div className="w-11/12 lg:w-full xl:w-3/5 flex flex-col items-center mt-8">
+          {/* <div className="w-11/12 lg:w-full xl:w-3/5 flex flex-col items-center mt-8">
             <button
               onClick={() => setShowRequest((val) => !val)}
               className="text-gray-500 text-sm text-center w-full mb-2"
@@ -253,7 +302,7 @@ const AppBody: React.FC<Props> = ({ state, setApiKey, createStream }) => {
                 </fieldset>
               </>
             )}
-          </div>
+          </div> */}
         </div>
       );
     default:
